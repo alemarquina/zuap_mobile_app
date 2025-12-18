22a4f55cf6251cdf684c6befa5270dca4f020f54
LucianMarquina <alemarquina789@gmail.com>
2025-12-18 15:02:50 -0500

    ref(core): Se añade cubit de Notificacions como dependencia

--- lib/core/providers/global_providers.dart
+++ lib/core/providers/global_providers.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zuap_mobile_app/core/di/injection_container.dart' as di;
 import 'package:zuap_mobile_app/features/battery/presentation/cubit/battery_cubit.dart';
+import 'package:zuap_mobile_app/features/notif/presentation/cubit/notifications_cubit.dart';
 import 'package:zuap_mobile_app/features/stations/presentation/cubit/station_cubit.dart';
 
 class GlobalProviders extends StatelessWidget {
@@ -15,6 +16,7 @@ class GlobalProviders extends StatelessWidget {
       providers: [
         BlocProvider<BatteryCubit>(create: (_) => di.sl<BatteryCubit>()),
         BlocProvider<StationCubit>(create: (_) => di.sl<StationCubit>()),
+        BlocProvider<NotificationsCubit>(create: (_) => di.sl<NotificationsCubit>())
       ],
       child: child,
     );
