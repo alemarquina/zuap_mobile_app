df16a6d8a5c18d98910af101fe19deef13a50ae3
LucianMarquina <alemarquina789@gmail.com>
2026-01-02 12:51:16 -0500

    feat: Añadiendo imagen de usuario y mejorando fragmento para evento de click

--- lib/features/profile/presentation/widgets/min_header_user.dart
+++ lib/features/profile/presentation/widgets/min_header_user.dart
@@ -11,39 +11,42 @@ class MiniHeaderUser extends StatefulWidget {
 class _MiniHeaderUserState extends State<MiniHeaderUser> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CircleAvatar(
-          radius: 30,
-          backgroundColor: Colors.grey[300],
-          child: const Icon(Icons.person, size: 35, color: Colors.grey),
-        ),
-        const SizedBox(width: 15),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Wade Warren',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w900,
-                  color: AppTheme.darkGrayColor,
+    return InkWell(      
+      onTap: () {
+        Navigator.pushNamed(context, '/user_profile_screen');
+      },
+      child: Row(
+        spacing: 15,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Image.network('https://cdn.filestackcontent.com/gVx98UWFQaKBWTGAjZD7', width: 55),
+          ),          
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Wade Warren',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w900,
+                    color: AppTheme.darkGrayColor,
+                  ),
+                ),                
+                Text(
+                  'Scooter 150',
+                  style: TextStyle(
+                    fontSize: 13,
+                    color: Colors.grey[600],
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 2),
-              Text(
-                'Scooter 150',
-                style: TextStyle(
-                  fontSize: 13,
-                  color: Colors.grey[600],
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
